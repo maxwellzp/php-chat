613e8bc8d395cac24d090cb06af17c966264cdb0
Maksim Evtyagin <evtyagin.maksim@gmail.com>
2025-03-30 15:06:19 +0300

    Make changes in rendering form

--- src/Form/MessageType.php
+++ src/Form/MessageType.php
@@ -4,8 +4,8 @@ namespace App\Form;
 
 use App\Entity\Message;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\ButtonType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,8 +14,19 @@ class MessageType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('content', TextareaType::class, ['attr' => ['autocomplete' => 'off']])
-            ->add('send', SubmitType::class)
+            ->add('content', TextType::class, [
+                'attr' => [
+                    'autocomplete' => 'off',
+                    'class' => 'form-control',
+                    'placeholder'=> "Type a message..."
+                ],
+                'label' => false
+            ])
+            ->add('send', ButtonType::class, [
+                'attr' => [
+                    'class' => 'btn btn-primary',
+                ],
+            ])
         ;
     }
 
@@ -23,6 +34,7 @@ class MessageType extends AbstractType
     {
         $resolver->setDefaults([
             'data_class' => Message::class,
+            'attr' => ['class' => 'mt-3']
         ]);
     }
 }
